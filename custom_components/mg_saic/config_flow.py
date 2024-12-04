bae99cb6dcc6a6b5d6081321962814a1206a5470
Alvaro Duarte <59612788+ad-ha@users.noreply.github.com>
2024-12-04 21:47:18 +0100

    Solve error 500 Configuration menu access

--- custom_components/mg_saic/config_flow.py
+++ custom_components/mg_saic/config_flow.py
@@ -190,6 +190,7 @@ class SAICMGOptionsFlowHandler(config_entries.OptionsFlow):
 
     def __init__(self, config_entry):
         """Initialize options flow."""
+        self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         """Manage the options."""
